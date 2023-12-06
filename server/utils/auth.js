217a2ab59c4df772f2363b3dfe42cb4cf173faec
DevJake99 <jacobg4@uci.edu>
2023-12-05 18:41:17 -0800

    moved files up one directory

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,14 @@
-const jwt = require(‘jsonwebtoken’);
-const secret = ‘mysecretssshhhhhhh’;
-const expiration = ‘2h’;
+const jwt = require('jsonwebtoken');
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
 module.exports = {
     authMiddleware: function ({ req }) {
         // allows token to be sent via req.body, req.query, or headers
         let token = req.body.token || req.query.token || req.headers.authorization;
         // [“Bearer”, “<tokenvalue>“]
         if (req.headers.authorization) {
-            token = token.split(' ’).pop().trim();
-    }
+            token = token.split(' ').pop().trim();
+        }
         if (!token) {
             return req;
         }
