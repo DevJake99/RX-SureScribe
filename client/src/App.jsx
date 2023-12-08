8035cc43d56dbb880ac5151ee7e5205ac52cd10b
Pa <137225158+pvang1987@users.noreply.github.com>
2023-12-07 20:35:15 -0600

    Added code to pharmdash, bootstrap code and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import PhysicianDashboard from './components/phycisianDashboard/PhysicianDashboard'
 import PhysicianProfile from './components/physicianProfile/PhysicianProfile'
 import PharmacistProfile from './components/pharmacistProfile/PharmacistProfile'
+import Login from './components/login/Login'
+import NavBar from './components/Navigation/NavBar';
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/logoin' element={<Login />} />
-        <Route path="/" element={<Layout></Layout>}>
+        <Route path="/" element={<Layout>
+          <Route path='/navbar' element={<NavBar />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/physician' element={<PhysicianDashboard />} />
           <Route path='/pharmacist' element={<PharmacistProfile />} />
+        </Layout>}>
         </Route>
       </Routes>
 
--- client/src/components/pharmacistDashboard/PharmacistDashboard.jsx
+++ client/src/components/pharmacistDashboard/PharmacistDashboard.jsx
@@ -1,10 +1,35 @@
-import './pharmacistDash.css'
+import { useState, useEffect } from 'react';
+import './pharmacistDash.css';
+import SearchIcon from '/Users/pavang/Desktop/RX-SureScribe/client/src/assets/search.svg';
+
 
 function PharmDash() {
-    return <>
+    const [searchName, setSearchName] = useState('');
+  
+    useEffect(() => {searchPatient(searchName)}, [searchName]);
+  
+    const searchPatient = async (patientName) => {
+      try {
+        const response = await fetch(`${patientName}`);
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching patient data:', error);
+      }
+    };
+  
+    return (
+      <>
         <h1>PharmDash</h1>
-
-    </>
-}
-
-export default PharmDash
+        <div className="search">
+          <input
+            placeholder="Search for patients"
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
+          />
+          {"\t"}{"\t"}{"\t"}<img src={SearchIcon} alt="Search" onClick={() => searchPatient(searchName)} />
+        </div>
+      </>
+    );
+  }
+export default PharmDash;
